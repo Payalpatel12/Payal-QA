3325fc25c4318076fbcbd3f0025e468ae2572b2a
Payalpatel12 <payal37.patel37@gmail.com>
2025-05-23 16:04:24 -0400

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,17 @@
+
+USERNAME = "Milly@gmail.com"
+PASSWORD = "StrongPassword123!"
+
+CARD_NUMBER = "1234 5678 9012 3456"
+EXPIRY = "12/2025" 
+CVC = "123"
+NAME_ON_CARD = "Payal Patel"
+
+FIRST_NAME = "Payal"
+LAST_NAME = "Patel"
+ADDRESS = "123 Abc St"
+APARTMENT = "Unit 123"
+CITY = "Toronto"
+STATE = "Ontario"
+ZIP = "L4A5B1"
+PHONE = "1234567890"
